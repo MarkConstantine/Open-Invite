fe8dc186cbf937bfdb4dc69de939b3dd9f774667
Mark Constantine <MarkConstantConstantine@gmail.com>
2020-04-27 13:51:35 -0700

    Eslint requiring semicolons

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,12 @@
 module.exports = {
   extends: "eslint:recommended",
-  "env": {
-    "browser": true,
-    "node": true,
-    "es6": true
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
   },
-  "parser": "babel-eslint",
+  parser: "babel-eslint",
+  rules: {
+    semi: [2, "always"],
+  },
 };
